c3a5c3f1a2c430742a33fa479a6d1dcb21039e50
MolliShearin <mollishearin@gmail.com>
2023-10-04 18:41:16 -0500

    The Basics- Angular Recipe Project

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
-
+recipes: Recipe[] = [
+  new Recipe("A Test Recipe", "This is simply a test", "https://images.unsplash.com/photo-1542010589005-d1eacc3918f2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cmVjaXBlfGVufDB8fDB8fHww&w=1000&q=80" )
+];
 }
